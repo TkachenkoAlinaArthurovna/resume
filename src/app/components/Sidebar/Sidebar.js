53980778d8c39bcf07bc35ece2473ee76c538557
Alina Tkachenko <tkachenkoalinaarthurovna@gmail.com>
2024-04-26 21:51:45 +0300

    added TypeScript

--- src/app/components/Sidebar/Sidebar.js
+++ src/app/components/Sidebar/Sidebar.js
@@ -48,6 +48,7 @@ export default function Sidebar({ children }) {
           <li>CSS</li>
           <li>SCSS</li>
           <li>JavaScript</li>
+          <li>TypeScript</li>
           <li>Gulp</li>
           <li>Git</li>
           <li>GitLab</li>
